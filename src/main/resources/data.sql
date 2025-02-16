c52a5e6b8da7191829b9d228b377f70f620606f9
shuvxm <kumarshubham5559@gmail.com>
2025-02-17 00:59:23 +0530

    "exploring docker with spring boot"

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO student(name, age) VALUES ('Sim', 43)
+INSERT INTO student(name, age) VALUES ('Modak', 3)
+INSERT INTO student(name, age) VALUES ('Doknoo', 63)
+INSERT INTO student(name, age) VALUES ('Shubham', 23)
